e12a4fc600dc245444a7f3ce7f81daea313a2043
Karebet Connec <karebetconnec@gmail.com>
2018-02-02 15:32:03 +0700

    Update Language_swicth.php

--- library/Language_swicth.php
+++ library/Language_swicth.php
@@ -3,43 +3,44 @@
 class language_swicth {
 	private $language_available =array('english','indonesia');
 	private $language_default ='english';
-	private $language_file =array('text');
-	//tambahkan di sini jika anda memiliki file bahasa,, misal email_lang.php
-	//private $language_file =array('text','email');
+	private $language_file =array('text','header_footer');
+	private $parameter_get ='lang';
+	private $parametersession ='multilanguage';
 	function __construct()
 	{
 		#parent::__construct();
+		#session_destroy();
 		$this->run_language();
 	}
 	public function run_language()
 	{
 		$CI =& get_instance();
-		$language = $this->language_available;
-		$language_swicthto = $CI->input->get('language_swicthto');
+		$language = $this->language_default;
+		$language_swicthto = $CI->input->get($this->parameter_get);
+		$parametersessioninrun = $this->parametersession;
 		$adaperubahan=false;
 		if ($language_swicthto) {
-			$array = array(
-				'language_swicthto' => $language_swicthto
-			);
 			if (in_array($language_swicthto, $this->language_available)) {
-				$CI->session->set_userdata( $array );
+				$CI->session->set_userdata( array('multilanguage'=> $language_swicthto));
 				$adaperubahan=true;
-				
 			}
 		}
-		$session_lang_swicth = $CI->session->userdata('language_swicthto');
+		$session_lang_swicth = $CI->session->userdata($this->parametersession);
 		if (!$session_lang_swicth) {
-			$array = array(
-				'language_swicthto' => $language
-			);
-			$CI->session->set_userdata( $array );
+			$CI->session->set_userdata( array('multilanguage' => $language));
 			$adaperubahan=false;
+			$language=$this->language_default ;
 		}else{
 			$language=$session_lang_swicth ;
 		}
+		$language_file=$this->language_file;
 		foreach ($language_file as $key) {
-			$CI->lang->load($key, $language);
+			if ($key!='') {
+				$CI->lang->load($key, $language);
+			}
 		}
-		
+		if ($adaperubahan==true) {
+			$CI->session->set_flashdata('toast', $CI->lang->line('toast_change_language_swicth', FALSE));
+		}
 	}
 }
